b0e7cb38284a3f6d4258669f700a875a16c02d7f
Ewa Kowalska <ewcia.kowalska@gmail.com>
2020-11-27 16:51:14 +0100

    Patch eslint plugin for sorting imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ const base = require('@polkadot/dev/config/eslint.cjs');
 // add override for any (a metric ton of them, initial conversion)
 module.exports = {
   ...base,
-  plugins: [...base.plugins, 'eslint-plugin-import', 'eslint-plugin-import-helpers'],
+  plugins: [...base.plugins, 'simple-import-sort'],
   ignorePatterns: [
     '.eslintrc.js',
     '.github/**',
@@ -28,18 +28,29 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     // this seems very broken atm, false positives
     '@typescript-eslint/unbound-method': 'off',
-    'import-helpers/order-imports': [
-      'warn',
-      {
-        groups: [
-          '/^i18next$/',
-          '/^((?!polkadot\/types$).)*\/types.*$/',
-          'module',
-          '/^@polkadot/',
-          ['parent', 'sibling', 'index'],
-          'absolute'
-        ]
+    'simple-import-sort/imports': [
+      'error',
+      { groups: [ 
+            ['^[^\/\\.]'],
+            ['^@polkadot'],
+            ['^\\.']
+        ],
       },
-    ],
+    ]
   }
+
+    // 'import-helpers/order-imports': [
+    //   'warn',
+    //   {
+    //     groups: [
+    //       '/^i18next|^@fortawesome\/fontawesome-svg-core/',
+    //       '/^((?!polkadot\/types(.*)).)*\/types.*|^@polkadot\/types\/types|@polkadot\/types\/interfaces|@polkadot\/apps-config\/settings\/endpoints|@polkadot\/types\/primitive\/types$/',
+    //       'module',
+    //       '/^@polkadot/',
+    //       ['parent', 'sibling', 'index'],
+    //       'absolute'
+    //     ]
+    //   },
+    // ],
+  
 };
